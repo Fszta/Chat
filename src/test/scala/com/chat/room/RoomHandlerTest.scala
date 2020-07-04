9c70e3418f0677907dfdc74c1ff24d09807106dd
ferszte <antoinefer@hotmail.com>
2020-07-04 13:44:39 +0200

    Test remove room when empty

--- src/test/scala/com/chat/room/RoomHandlerTest.scala
+++ src/test/scala/com/chat/room/RoomHandlerTest.scala
@@ -3,9 +3,8 @@ package com.chat.room
 import akka.actor.{ActorRef, Props}
 import akka.http.scaladsl.testkit.{ScalatestRouteTest, WSProbe}
 import akka.util.Timeout
-import com.chat.room.Events.{CreateRoomIfNotExists, GetRooms, GetUsers, User}
+import com.chat.room.Events.{CreateRoomIfNotExists, GetRooms, GetUsers, LeaveRoom, User}
 import org.scalatest.{FunSuite, Matchers}
-
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import akka.pattern.ask
@@ -62,7 +61,6 @@ class RoomHandlerTest extends FunSuite with Matchers with ScalatestRouteTest {
     assert(rooms.contains(idTestRoom))
   }
 
-
   test("should create a room on first user connection") {
     implicit val timeout = Timeout(1.seconds)
     val testRoomId = 2
@@ -78,6 +76,30 @@ class RoomHandlerTest extends FunSuite with Matchers with ScalatestRouteTest {
 
       // Room with id testRoomId shoul exists & number of room 1
       assert(existingRooms.size == 1 && existingRooms.contains(testRoomId))
+    }
+  }
+
+  test("should remove room when all user are disconnected") {
+    implicit val timeout = Timeout(1.seconds)
+    val testRoomId = 5
+    val wsClient = WSProbe()
+    val roomHandler = system.actorOf(Props(new RoomHandler))
+    val chat = new Chat(roomHandler)
+
+    WS(s"/api?userName=Tester&roomId=$testRoomId", wsClient.flow) ~> chat.wsRoute ~> check {
+      val futureGetRooms = roomHandler ? GetRooms
+      val rooms = Await.result(futureGetRooms, timeout.duration).asInstanceOf[scala.collection.mutable.Map[Int, ActorRef]]
+      val futureGetUsers = rooms(testRoomId) ? GetUsers
+      val users = Await.result(futureGetUsers, timeout.duration).asInstanceOf[scala.collection.mutable.Map[User, ActorRef]]
+      rooms(testRoomId) ! LeaveRoom(users.keys.head)
+
+      // For testing, wait that room has been deleted
+      Thread.sleep(500)
+
+      // Check room has been removed
+      val futureGetRoomsAfterAllUserSignout = roomHandler ? GetRooms
+      val roomsAfterAllUsersSignout = Await.result(futureGetRoomsAfterAllUserSignout,timeout.duration).asInstanceOf[scala.collection.mutable.Map[Int,ActorRef]]
+      assert(!roomsAfterAllUsersSignout.contains(testRoomId))
     }
   }
 }
