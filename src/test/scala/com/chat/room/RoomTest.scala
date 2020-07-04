0a0580c71b34d6bb743f86f6fd9a9598aae1587f
ferszte <antoinefer@hotmail.com>
2020-07-04 13:42:30 +0200

    Send message test

--- src/test/scala/com/chat/room/RoomTest.scala
+++ src/test/scala/com/chat/room/RoomTest.scala
@@ -3,12 +3,14 @@ package com.chat.room
 import akka.actor.{ActorRef, Props}
 import akka.http.scaladsl.testkit.{ScalatestRouteTest, WSProbe}
 import akka.util.Timeout
-import com.chat.room.Events._
+import akka.pattern.ask
 import org.scalatest.{FunSuite, Matchers}
 import scala.concurrent.duration._
 import scala.concurrent.Await
-import akka.pattern.ask
+import com.chat.room.Events._
+import com.chat.utils.Formater.messageToJsonStr
 import com.chat.Chat
+
 
 class RoomTest extends FunSuite with Matchers with ScalatestRouteTest {
   test("should create a room") {
@@ -33,7 +35,7 @@ class RoomTest extends FunSuite with Matchers with ScalatestRouteTest {
     assert(connectedUsers.size == 0)
   }
 
-  test("should add an user a new Room on ws connection"){
+  test("should add an user a new Room on ws connection") {
     implicit val timeout = Timeout(1.seconds)
     val roomHandler = system.actorOf(Props(new RoomHandler))
     val chat = new Chat(roomHandler)
@@ -42,10 +44,47 @@ class RoomTest extends FunSuite with Matchers with ScalatestRouteTest {
 
     WS(s"/api?userName=Tester&roomId=$testRoomId", wsClient.flow) ~> chat.wsRoute ~> check {
       val futureGetRooms = roomHandler ? GetRooms
-      val rooms = Await.result(futureGetRooms, timeout.duration).asInstanceOf[scala.collection.mutable.Map[Int,ActorRef]]
-      rooms(testRoomId) ? GetUsers
-      val users = Await.result(futureGetRooms, timeout.duration).asInstanceOf[scala.collection.mutable.Map[User,ActorRef]]
-      assert(users.size==1)
+      val rooms = Await.result(futureGetRooms, timeout.duration).asInstanceOf[scala.collection.mutable.Map[Int, ActorRef]]
+      val futureGetUsers = rooms(testRoomId) ? GetUsers
+      val users = Await.result(futureGetUsers, timeout.duration).asInstanceOf[scala.collection.mutable.Map[User, ActorRef]]
+      assert(users.size == 1)
+    }
+  }
+
+  test("leaveRoom") {
+    implicit val timeout = Timeout(1.seconds)
+    val testRoomId = 2
+    val wsClient = WSProbe()
+    val roomHandler = system.actorOf(Props(new RoomHandler))
+    val chat = new Chat(roomHandler)
+
+    WS(s"/api?userName=Tester&roomId=$testRoomId", wsClient.flow) ~> chat.wsRoute ~> check {
+      val futureGetRooms = roomHandler ? GetRooms
+      val rooms = Await.result(futureGetRooms, timeout.duration).asInstanceOf[scala.collection.mutable.Map[Int, ActorRef]]
+      val futureGetUsers = rooms(testRoomId) ? GetUsers
+      val users = Await.result(futureGetUsers, timeout.duration).asInstanceOf[scala.collection.mutable.Map[User, ActorRef]]
+      rooms(testRoomId) ! LeaveRoom(users.keys.head)
+      val futureGetUsersAfterLeave = rooms(testRoomId) ? GetUsers
+      val usersAfterLeaveRoom = Await.result(futureGetUsersAfterLeave, timeout.duration).asInstanceOf[scala.collection.mutable.Map[User, ActorRef]]
+      assert(usersAfterLeaveRoom.size == 0)
+    }
+  }
+
+  test("shoud send a message & receive it back") {
+    implicit val timeout = Timeout(1.seconds)
+    val roomHandler = system.actorOf(Props(new RoomHandler))
+    val chat = new Chat(roomHandler)
+    val wsClient = WSProbe()
+    val testRoomId = 2
+    val actualTimestamp = java.lang.System.currentTimeMillis()
+    val expectedtestMessage = messageToJsonStr(Message("Tester", "test message",actualTimestamp))
+
+    WS(s"/api?userName=Tester&roomId=$testRoomId", wsClient.flow) ~> chat.wsRoute ~> check {
+      wsClient.expectMessage("Successfully connected")
+      val futureGetRooms = roomHandler ? GetRooms
+      val rooms = Await.result(futureGetRooms, timeout.duration).asInstanceOf[scala.collection.mutable.Map[Int, ActorRef]]
+      rooms(testRoomId) ! SendMessage("Tester", "test message", actualTimestamp)
+      wsClient.expectMessage(expectedtestMessage)
     }
   }
 }
