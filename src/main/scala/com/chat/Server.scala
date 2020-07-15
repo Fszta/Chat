7eb99f9d5f0715ec7adc079ae0d25b73e6e122e0
ferszte <antoinefer@hotmail.com>
2020-07-15 20:16:16 +0200

    Add html ressource

--- src/main/scala/com/chat/Server.scala
+++ src/main/scala/com/chat/Server.scala
@@ -2,10 +2,11 @@ package com.chat
 
 import akka.actor.{ActorSystem, Props}
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Route
 import akka.stream.ActorMaterializer
 import com.chat.room.RoomHandler
 import com.chat.utils.{Configuration, Log}
-
+import akka.http.scaladsl.server.Directives._
 
 object Server extends Configuration with Log {
   implicit val system = ActorSystem()
@@ -15,7 +16,13 @@ object Server extends Configuration with Log {
     val serverConfig = loadConfiguration
     val roomHandler = system.actorOf(Props(new RoomHandler))
     val chat = new Chat(roomHandler)
+
+    val webAppRoute: Route =
+      pathEndOrSingleSlash {
+        getFromResource("web-app/chat.html")
+      } ~ getFromResourceDirectory("web-app")
+
     writeLog("info", s"start http server at ${serverConfig.host}:${serverConfig.port}")
-    Http().bindAndHandle(chat.wsRoute, serverConfig.host, serverConfig.port)
+    Http().bindAndHandle(chat.wsRoute~webAppRoute, serverConfig.host, serverConfig.port)
   }
 }
